c1beadc3056f14a26a4957426a4b58dc3491956a
wei <wei@localhost>
2017-06-04 23:56:10 +0900

    n

---
+++ ShadowDancer/source/Component/OmniCamera.cpp
@@ -0,0 +1,18 @@
+/*
+ * OmniCamera.cpp
+ *
+ *  Created on: Jun 4, 2017
+ *      Author: wei
+ */
+
+#include "OmniCamera.h"
+
+OmniCamera::OmniCamera() {
+	// TODO Auto-generated constructor stub
+
+}
+
+OmniCamera::~OmniCamera() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ ShadowDancer/source/Component/OmniCamera.h
@@ -0,0 +1,17 @@
+/*
+ * OmniCamera.h
+ *
+ *  Created on: Jun 4, 2017
+ *      Author: wei
+ */
+
+#ifndef COMPONENT_OMNICAMERA_H_
+#define COMPONENT_OMNICAMERA_H_
+
+class OmniCamera {
+public:
+	OmniCamera();
+	virtual ~OmniCamera();
+};
+
+#endif /* COMPONENT_OMNICAMERA_H_ */
