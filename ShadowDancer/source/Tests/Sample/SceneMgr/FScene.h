88651c5c5b119ed8429e58550c410a770d92e797
wei <wei@localhost>
2017-06-12 09:21:41 +0900

    n

--- ShadowDancer/source/Tests/Sample/SceneMgr/FScene.h
+++ ShadowDancer/source/Tests/Sample/SceneMgr/FScene.h
@@ -8,9 +8,9 @@
 #ifndef SCENEMGR_FSCENE_H_
 #define SCENEMGR_FSCENE_H_
 
+#include "../../../Common/Object/Block.h"
+#include "../../../Common/Object/Wall.h"
 #include "../Actor/Robot.h"
-#include "../Object/Wall.h"
-#include "../Object/Block.h"
 
 class FScene : public assa2d::SceneMgr {
 public:
