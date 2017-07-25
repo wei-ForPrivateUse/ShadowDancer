04c0f6d0c7839c63b9803d8d64ea6c0270921f2e
wei-ForPrivateUse <931995113@qq.com>
2017-07-25 17:13:43 +0900

    n

--- ShadowDancer/source/Tests/J1/J1_Scene.h
+++ ShadowDancer/source/Tests/J1/J1_Scene.h
@@ -14,12 +14,12 @@
 ///scene.
 class J1_Scene : public assa2d::SceneMgr {
 public:
-	J1_Scene(Configuration* conf, ANNWeights* weights,
-			std::pair<float32, float32> goodfood={4.0f, 0.4f}, std::pair<float32, float32> badfood={2.0f, 1.0f});
-	virtual ~J1_Scene() { }
-
-	const std::list<assa2d::Node*>*  m_food_list;
-	PNest* m_nest;
+//	J1_Scene(Configuration* conf, ANNWeights* weights,
+//			std::pair<float32, float32> goodfood={4.0f, 0.4f}, std::pair<float32, float32> badfood={2.0f, 1.0f});
+//	virtual ~J1_Scene() { }
+//
+//	const std::list<assa2d::Node*>*  m_food_list;
+//	PNest* m_nest;
 };
 
 #endif /* TESTS_J1_J1_SCENE_H_ */
